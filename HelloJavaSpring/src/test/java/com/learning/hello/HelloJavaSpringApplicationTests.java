38f09f6b780ff4eb424cda20f8d1782fc507853e
Aldiransyah Rizky Putra <87604327+aldirarizkyp@users.noreply.github.com>
2022-06-27 19:26:52 +0700

    push JavaSpringHello

---
+++ HelloJavaSpring/src/test/java/com/learning/hello/HelloJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.hello;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloJavaSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
